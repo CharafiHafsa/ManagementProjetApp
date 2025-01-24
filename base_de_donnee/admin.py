9090ac7d284705f171086ecb3ea21aeb10e9971a
Hafsa <ms.charafihafsa@gmail.com>
2025-01-24 16:42:06 +0100

    merge base de donnees et login

---
+++ base_de_donnee/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Avoir
+from .models import Classe
+from .models import Composer
+from .models import Construire
+from .models import Creer
+from .models import Etudiant
+from .models import Groupe
+from .models import Professeur
+from .models import Project
+from .models import Todoliste
+
+admin.site.register(Avoir)
+admin.site.register(Classe)
+admin.site.register(Composer)
+admin.site.register(Construire)
+admin.site.register(Creer)
+admin.site.register(Etudiant)
+admin.site.register(Groupe)
+admin.site.register(Professeur)
+admin.site.register(Project)
+admin.site.register(Todoliste)
