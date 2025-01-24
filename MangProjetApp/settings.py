61830f75a17c8b834cfcbc5aa23473cc4cdc04fb
Hafsa <ms.charafihafsa@gmail.com>
2025-01-24 01:30:17 +0100

    add static folder

--- MangProjetApp/settings.py
+++ MangProjetApp/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES =[
+    os.path.join(BASE_DIR, 'MangProjetApp/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
